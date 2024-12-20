8b9b730b3a0bdb96fd777339ec3ff9e1d4e2077a
Gabriel Baril <gabriel-baril2010@hotmail.com>
2024-12-20 16:34:06 -0500

    Fixed folder structure

---
+++ imgui.sharpmake.cs
@@ -0,0 +1,24 @@
+using System.IO; // For Path.Combine
+using Sharpmake; // Contains the entire Sharpmake object library.
+
+[Generate]
+public class ImguiProject : BaseCppProject
+{
+    public ImguiProject()
+    {
+        Name = "imgui";
+        SourceRootPath = @"[project.SharpmakeCsPath]\src";
+        AddTargets(TargetUtil.DefaultTarget);
+    }
+    
+    [Configure]
+    public new void ConfigureAll(Project.Configuration conf, Target target)
+    {
+        base.ConfigureAll(conf, target);
+        conf.SolutionFolder = Constants.EXTERNAL_VS_CATEGORY;
+        conf.Output = Project.Configuration.OutputType.Lib;
+        conf.TargetPath = @"[project.SharpmakeCsPath]\Out\Bin\[target.Platform]-[target.Optimization]";
+        conf.IntermediatePath = @"[project.SharpmakeCsPath]\Out\Intermediate\[target.Platform]-[target.Optimization]";
+        conf.IncludePaths.Add(@"[project.SharpmakeCsPath]\src\");
+    }
+}
