83f5d48a650d4aea85de87c720f67df14ca3bb84
Gabriel Baril <gabriel-baril2010@hotmail.com>
2025-10-25 23:48:00 -0400

    Update imgui.sharpmake.cs

--- imgui.sharpmake.cs
+++ imgui.sharpmake.cs
@@ -1,11 +1,11 @@
 using System.IO; // For Path.Combine
 using Sharpmake; // Contains the entire Sharpmake object library.
 [Generate]
-public class ImguiProject : BaseCppProject
+public class HdnCodeExternalImguiProject : BaseCppProject
 {
-    public ImguiProject()
+    public HdnCodeExternalImguiProject()
     {
-        Name = "imgui";
+        Name = "hdn.code.external.imgui";
         SourceRootPath = @"[project.SharpmakeCsPath]\src";
         AddTargets(TargetUtil.DefaultTarget);
     }
